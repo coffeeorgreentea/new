5c79619a3737beb937ed598c60020d45d9350dd8
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-17 06:19:09 -0500

    delete app router

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -4,13 +4,6 @@ import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server';
 import superjson from 'superjson';
 import { type AppRouter } from 'api';
 
-const getBaseUrl = () => {
-  if (typeof window !== 'undefined') return ''; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 7000}`; // dev SSR should use localhost
-};
-
-/** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
   config() {
     return {
@@ -33,7 +26,7 @@ export const api = createTRPCNext<AppRouter>({
             (opts.direction === 'down' && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/trpc`,
         }),
       ],
     };
