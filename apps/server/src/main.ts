cedc8fc7dc3752fd9ac7458fb4d9e262fd9ffe99
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-17 06:20:35 -0500

    add cors to server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,6 +7,7 @@ import { createTRPCContext } from 'api';
 import { appRouter, type AppRouter } from 'api';
 import { clerkPlugin } from '@clerk/fastify';
 import ws from '@fastify/websocket';
+import cors from '@fastify/cors';
 
 const host = process.env.SERVER_HOST ?? 'localhost';
 const port = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 7100;
@@ -15,6 +16,13 @@ const port = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 7100;
 const server = Fastify({
   logger: process.env.NODE_ENV !== 'production',
   maxParamLength: 5000,
+});
+
+server.register(cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 });
 
 server.register(clerkPlugin);
@@ -30,7 +38,6 @@ server.register(fastifyTRPCPlugin, {
     onError({ path, error }) {
       console.error(`Error in tRPC handler on path '${path}':`, error);
     },
-    
   } satisfies FastifyTRPCPluginOptions<AppRouter>['trpcOptions'],
 });
 
