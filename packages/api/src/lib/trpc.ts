97a9616df0a374628b1784ecc1ba39f51acf06cd
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-17 02:19:14 -0500

    trpc context with clerk auth

---
+++ packages/api/src/lib/trpc.ts
@@ -0,0 +1,44 @@
+import { initTRPC, TRPCError } from '@trpc/server';
+import type { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify';
+import superjson from 'superjson';
+import { ZodError } from 'zod';
+import { db } from '@slated/db';
+import { getAuth } from '@clerk/fastify';
+
+export const createTRPCContext = async (opts: CreateFastifyContextOptions) => {
+  return {
+    req: opts.req,
+    db,
+    auth: getAuth(opts.req),
+  };
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter: ({ shape, error }) => ({
+    ...shape,
+    data: {
+      ...shape.data,
+      zodError: error.cause instanceof ZodError ? error.cause.flatten() : null,
+    },
+  }),
+});
+
+export const createCallerFactory = t.createCallerFactory;
+
+export const createTRPCRouter = t.router;
+
+const enforceClerk = t.middleware(({ next, ctx }) => {
+  if (!ctx.auth.userId) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+  return next({
+    ctx: {
+      auth: ctx.auth,
+    },
+  });
+});
+
+export const publicProcedure = t.procedure;
+
+export const protectedProcedure = t.procedure.use(enforceClerk);
